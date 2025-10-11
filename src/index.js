410fe934a6d8cfd04478934568e0646139c8871a
FedeManzano <154226686+FedeManzano@users.noreply.github.com>
2025-10-11 16:38:55 -0300

    refact: código del src, refactorizado, archivos innecesarios borrados.

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import  "./bodystyle"
 import "./cargaMenues"
 import"./copiarCodigo"
 
-    window.onload = () => {
-              setTimeout(function () {
-            BS.DropDownInit()
-            BS.CodigoHtmlInit()
-            BS.CodigoJsInit()
-            BS.ToolTipsInit()
-            BS.NavInit(idMenu)
-            BS.BotonInicioInit()
-            $("#sidebar").fadeIn(1500)
-            }, 50); 
-        }
+window.onload = () => {
+  setTimeout( () => {
+    BS.DropDownInit()
+    BS.CodigoHtmlInit()
+    BS.CodigoJsInit()
+    BS.ToolTipsInit()
+    BS.NavInit(idMenu)
+    BS.BotonInicioInit()
+    $("#sidebar").fadeIn(1500)
+    }, 50); 
+}
 
-        CargarMenu.Iniciar()
-        Copiar.Iniciar()
+CargarMenu.Iniciar()
+Copiar.Iniciar()
