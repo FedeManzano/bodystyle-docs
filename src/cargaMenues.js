2a383765b68c302390ee2f90d1e4a5fbc2172635
FedeManzano <154226686+FedeManzano@users.noreply.github.com>
2025-10-17 01:24:54 -0300

    docs: agregada la documentación de la grilla, comienzo de la documentación de la grilla, terminando flexbox.

--- src/cargaMenues.js
+++ src/cargaMenues.js
@@ -7,63 +7,63 @@ import $ from "jquery"
         
     $("#sidebar").html
     (
-    `       <a  class="titulo c-blanco mar-2 t-may" data-target="#m1">Iniciación</a>
-            <ul id="m1">
-                <li><a href="../index.html">Home</a></li>
-                <li><a href="get_started.html">Get Started</a></li>
-                <li><a href="medidas.html" >Medidas</a></li>
-                <li><a href="colores.html">Colores</a></li>
-                <li><a href="tablas.html">Tablas</a></li>
-                <li><a href="texto.html">Texto</a></li>
-                <li><a href="opacidad.html">Opacidad</a></li>
-                <li><a href="bordes.html">Bordes</a></li>
-            </ul>
-            <a  class="titulo c-blanco t-may" data-target="#m2">CSS</a>
-            <ul id="m2">
-                <li><a href="botones.html">Botones</a></li>
-                <li><a href="grupo_botones.html">Grupo Botones</a></li>
-                <li><a href="badges.html">Badges</a></li>
-                <li><a href="alertas.html">Alertas</a></li>
-                <li><a href="tarjetas.html">Tarjetas</a></li>
-                <li><a href="breadcrumbs.html">Breadcrumbs</a></li>
-                <li><a href="colecciones.html">Colecciones</a></li>
-                <li><a href="">Etiquetas</a></li>
-                <li><a href="">Decoradores</a></li>
-                <li><a href="">Footers</a></li>
-            </ul>
-            <a  class="titulo c-blanco t-may" data-target="#m3">Alineamiento</a>
-            <ul id="m3">
-                <li><a href="contenedor.html">Contenedor</a></li>
-                <li><a href="">Helpers</a></li>
-                <li><a href="margin.html">Margin</a></li>
-                <li><a href="padding.html">Padding</a></li>
-                <li><a href="flexbox.html">Flexbox</a></li>
-                <li><a href="">Grilla</a></li>
-            </ul>
-            <a  class="titulo c-blanco t-may" data-target="#m4">Formularios</a>
-            <ul id="m4">
-                <li><a href="">Input</a></li>
-                <li><a href="">Range</a></li>
-                <li><a href="">Select</a></li>
-                <li><a href="">Grupos</a></li>
-                <li><a href="">Checkbox</a></li>
-                <li><a href="">Radio</a></li>
-                <li><a href="">Formularios</a></li>
-                <li><a href="">Template</a></li>
-            </ul>
-            <a  class="titulo c-blanco t-may" data-target="#m5">JS</a>
-            <ul id="m5">
-                <li><a href="">Boton Inicio</a></li>
-                <li><a href="">Boton Flotante</a></li>
-                <li><a href="">Colección Flotante</a></li>
-                <li><a href="">Dropdown</a></li>
-                <li><a href="">ToolTips</a></li>
-                <li><a href="">Comentarios</a></li>
-                <li><a href="">Show Code</a></li>
-                <li><a href="">Toasts</a></li>
-                <li><a href="">Modals</a></li>
-                <li><a href="">Tabs</a></li>
-            </ul>`
+    `   <a  class="titulo c-blanco mar-2 t-may" data-target="#m1">Iniciación</a>
+        <ul id="m1">
+            <li><a href="../index.html">Home</a></li>
+            <li><a href="get_started.html">Get Started</a></li>
+            <li><a href="medidas.html" >Medidas</a></li>
+            <li><a href="colores.html">Colores</a></li>
+            <li><a href="tablas.html">Tablas</a></li>
+            <li><a href="texto.html">Texto</a></li>
+            <li><a href="opacidad.html">Opacidad</a></li>
+            <li><a href="bordes.html">Bordes</a></li>
+        </ul>
+        <a  class="titulo c-blanco t-may" data-target="#m2">CSS</a>
+        <ul id="m2">
+            <li><a href="botones.html">Botones</a></li>
+            <li><a href="grupo_botones.html">Grupo Botones</a></li>
+            <li><a href="badges.html">Badges</a></li>
+            <li><a href="alertas.html">Alertas</a></li>
+            <li><a href="tarjetas.html">Tarjetas</a></li>
+            <li><a href="breadcrumbs.html">Breadcrumbs</a></li>
+            <li><a href="colecciones.html">Colecciones</a></li>
+            <li><a href="">Etiquetas</a></li>
+            <li><a href="">Decoradores</a></li>
+            <li><a href="">Footers</a></li>
+        </ul>
+        <a  class="titulo c-blanco t-may" data-target="#m3">Alineamiento</a>
+        <ul id="m3">
+            <li><a href="contenedor.html">Contenedor</a></li>
+            <li><a href="">Helpers</a></li>
+            <li><a href="margin.html">Margin</a></li>
+            <li><a href="padding.html">Padding</a></li>
+            <li><a href="flexbox.html">Flexbox</a></li>
+            <li><a href="grid.html">Grid</a></li>
+        </ul>
+        <a  class="titulo c-blanco t-may" data-target="#m4">Formularios</a>
+        <ul id="m4">
+            <li><a href="">Input</a></li>
+            <li><a href="">Range</a></li>
+            <li><a href="">Select</a></li>
+            <li><a href="">Grupos</a></li>
+            <li><a href="">Checkbox</a></li>
+            <li><a href="">Radio</a></li>
+            <li><a href="">Formularios</a></li>
+            <li><a href="">Template</a></li>
+        </ul>
+        <a  class="titulo c-blanco t-may" data-target="#m5">JS</a>
+        <ul id="m5">
+            <li><a href="">Boton Inicio</a></li>
+            <li><a href="">Boton Flotante</a></li>
+            <li><a href="">Colección Flotante</a></li>
+            <li><a href="">Dropdown</a></li>
+            <li><a href="">ToolTips</a></li>
+            <li><a href="">Comentarios</a></li>
+            <li><a href="">Show Code</a></li>
+            <li><a href="">Toasts</a></li>
+            <li><a href="">Modals</a></li>
+            <li><a href="">Tabs</a></li>
+        </ul>`
     )
 
     if(enlace !== null && enlace !== undefined)
