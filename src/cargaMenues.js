4360487c7e56b4864bd368f0eeb45c4fe22a1d6d
FedeManzano <154226686+FedeManzano@users.noreply.github.com>
2025-11-06 11:23:32 -0300

    docs: actualziada la información bpasica del versionado en los elementos dinámicos y la página get started.

--- src/cargaMenues.js
+++ src/cargaMenues.js
@@ -84,7 +84,7 @@ import "./bodystyle"
         <div  class="bs-nav-md align-right-list">
             <a  class="btn-menu"></a>
             <a  href="#"  class="logo-container">
-                <img  src="../logo/logo.png"  alt="Foto de perfil">
+                <img class='bor-rad-por-50' src="../imagenes/20191106_205049.png"  alt="Foto de perfil">
             </a>
             <div  class="ocultar-desde-medianos">
                 <ul>
@@ -93,8 +93,8 @@ import "./bodystyle"
                 </ul>
             </div>
             <div  class="right-content">
-                <label  class="badge-mje-right badge-mje-bodyui mr-2 mb-1"><i class="fz-16 bs-tag">&nbsp;</i>v4.8.0</label>
-                <a href="" class="btn-sm-o btn-white-o bor-pill">Descargar</a>
+                <label  class="badge-mje-right badge-mje-bodyui mr-2 mb-1"><i class="fz-16 bs-tag">&nbsp;</i>v5.0.0</label>
+                <a href="" class="btn-sm-o btn-white-o bor-pill disparador" data-info="Versión Transpilada y procesada de bodystyle 5.0.0">Descargar</a>
             </div>
         </div>
         `
@@ -116,20 +116,20 @@ import "./bodystyle"
         <div class="ancho-55 ancho-m-75 ancho-s-90 ancho-xs-100">
         <div class="card">
             <div class="card-simple-dark card-cover">
-                <h4>Descargar Bodystyle<span class="badge badge-bodyui"><i class="bs-tag c-white">&nbsp;</i>4.8.0</span></h4>
-                <small>Versión 4.8.0</small>
+                <h4>Descargar Bodystyle<span class="badge badge-bodyui"><i class="bs-tag c-white">&nbsp;</i>5.0.0</span></h4>
+                <small>Versión 5.0.0</small>
                 <p>
                     Con el botón que está abajo de este texto descargará
                     la versión procesada de la librería en su 
                     <a href='https://github.com/FedeManzano/bodystyle' target='_blank' class='link tips-ele '
                     data-tips="Versión procesada de Bodystyle"
-                    data-pos='right'>versión 4.8.0</a>, el segundo botón es para descargar esta misma documentación
+                    data-pos='right'>versión 5.0.0</a>, el segundo botón es para descargar esta misma documentación
                     para no tener que ver publicidad que el hosting introduce sin permiso.
                 </p> 
                 <a target="_blank" href="https://mega.nz/file/UZEzQI5K#9ULEwt4p7DlILAaXz8Mu5z2O-Rsgr-7V360fWUxqs58" class="btn-cover-sm fd-bodyui mar-2 disparador bor-pill"
-                data-info="<i class='fa-solid fa-file-zipper c-orange'>&nbsp</i>bodystyle_v4.8.0.zip" data-pos='right'>Descargar</a>
+                data-info="<i class='fa-solid fa-file-zipper c-orange'>&nbsp</i>bodystyle_v5.0.0.zip" data-pos='right'>Descargar</a>
                 <a target="_blank" href="https://mega.nz/file/4c1ggChQ#uPpobno10HaNf1il6-5KQu2ZL14WxJug75dGTCr3cHo" class="btn-cover-sm-o btn-white-o mar-1 tips-ele bor-pill"
-                data-tips="Descarga de esta misma documentación.">Docs 1.8.0</a>
+                data-tips="Descarga de esta misma documentación.">Docs 2.0.0</a>
             </div>
         </div> 
         </div>
